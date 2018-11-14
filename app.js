572ba34fcf6bb4c7e46fcf89b70a4296b5602e3b
Joao Cardoso <joao.ccardoso@gmail.com>
2018-11-14 22:05:41 +0000

    Issue5 developed 20181114

--- app.js
+++ app.js
@@ -16,20 +16,39 @@ const uuid = require('uuid/v4');
 //session handling and store
 const session = require('express-session');
 const FileStore = require('session-file-store')(session);
+//configure Passport
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
 
+const users = [
+    {id: '2f24vvg', email: 'test@test.com', password: 'password'}
+];
+  
+// configure passport.js to use the local strategy
+passport.use(new LocalStrategy(
+    { usernameField: 'email' },
+    (email, password, done) => {
+        //console.log('Inside local strategy callback');
+        // here is where you make a call to the database
+        // to find the user based on their username or email address
+        // for now, we'll just pretend we found that it was users[0]
+        const user = users[0];
+        if(email === user.email && password === user.password) {
+            //log.info('Logon successful: ' + user.email);
+            return done(null, user);
+        }
+    }
+));
+
+// tell passport how to serialize the user
+passport.serializeUser((user, done) => {
+    console.log('Inside serializeUser callback. User id is save to the session file store here');
+    done(null, user.id);
+});
+    
 var app = express();
 
-// add and configure session middleware
-app.use(session({
-    genid: (req) => {
-      //console.log('Inside the session middleware')
-      //log.info(req.sessionID);
-       return uuid(); // use UUIDs for session IDs
-    },
-    secret: credentials.cookieSecret,
-    resave: false,
-    saveUninitialized: true
-  }));
+
 
 /*
 //middleware should positioned before router
@@ -81,6 +100,20 @@ var persons = [
     }
 ];
 
+// add and configure session middleware
+app.use(session({
+    genid: (req) => {
+      //console.log('Inside the session middleware')
+      //log.info(req.sessionID);
+       return uuid(); // use UUIDs for session IDs
+    },
+    secret: credentials.cookieSecret,
+    resave: false,
+    saveUninitialized: true
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.get('/',function(req,res){
     //res.send('Hello e-gov');
     //res.json(persons);
@@ -92,6 +125,30 @@ app.get('/',function(req,res){
     });
 });
 
+// create the login get and post routes  
+app.get('/login',function(req,res){
+    res.render('login', {
+        action: 'login',
+        persons: persons
+    });
+});
+
+app.post('/login', (req, res, next) => {
+    //console.log('Inside POST /login callback')
+    passport.authenticate('local', (err, user, info) => {
+      //console.log('Inside passport.authenticate() callback');
+      //console.log(`req.session.passport: ${JSON.stringify(req.session.passport)}`)
+      log.info(`req.user: ${JSON.stringify(req.user)}`);
+      req.login(user, (err) => {
+        //console.log('Inside req.login() callback')
+        log.info(`Session id started: ${JSON.stringify(req.session.passport)}`);
+        log.info(`User logged in: ${JSON.stringify(req.user)}`);
+        return res.send('You were authenticated & logged in!\n');
+      })
+    })(req, res, next);
+  })
+  
+
 app.get('/contactfeedback',function(req,res){
     //res.send('Hello e-gov');
     //res.json(persons);
@@ -99,13 +156,12 @@ app.get('/contactfeedback',function(req,res){
          action: 'home',
          persons: persons
      });
- });
+});
 
  app.get(('/' + credentials.urlpaths.plugins + ':name'),function(req,res){
     //download xml file
     var file = __dirname + '/' + credentials.urlpaths.plugins + req.params.name
     var file = file.replace("/","\\");
-    console.log(file);
     res.download(file); // Set disposition and send it.
     log.info('plug-in download: ' + file);
 });
