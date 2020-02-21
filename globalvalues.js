38eb75713fee03e41fe887562d8018fa68a96253
Joao Cardoso <joao.ccardoso@gmail.com>
2020-02-21 10:08:50 +0000

    Issues #89, #90 and #91 solved 20200221

--- globalvalues.js
+++ globalvalues.js
@@ -1,7 +1,7 @@
 module.exports = {
     Recommendation: {
         Priority: 'Urgent|Regular',
-        RiskCharacterization: 'Governance|Operational|Reputational|Financial|Compliance|Misappropriation of assets|Obsolescence',
+        RiskCharacterization: 'Governance|Operational|Reputational|Financial|Compliance|Misappropriation of assets',
         RiskEvaluation: 'State|Region|Municipality|Activity Area|Entity',
         Importance: 'Strategic|Important, but not essential',
         ImplementationStatus: 'Implemented|Started Implementation|Partially implemented|Final Stage Implementation|Not started Implementation|Not addressed|No longer applicable'
